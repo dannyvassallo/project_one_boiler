85cfaf9cad6609e08c1bf6740590a2d79cd833e6
J Suc <jsucameli87@gmail.com>
2019-04-04 21:05:55 -0400

    added a weather widget

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,5 @@
-/* eslint-disable */
-console.warn('Project One JS Initialized');
-/* eslint-enable */
+window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = []; window.myWidgetParam.push({
+  id: 18, cityid: '', appid: 'e1eb6cf9d4bcb08d8495b7bd15e2947f', units: 'imperial', containerid: 'openweathermap-widget-18',
+}); (function () { const script = document.createElement('script'); script.async = true; script.charset = 'utf-8'; script.src = '//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js'; const s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s); }());
+
+
