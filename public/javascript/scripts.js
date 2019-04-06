fc39421bd10db5fc9670715605efc60abca4d48e
JoeySuc89 <jsucameli1@gmail.com>
2019-04-06 10:38:07 -0400

    initialize firebase

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,12 +1,10 @@
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCiLgK-ruR_RYem27X328H3H9bGzujRjn8",
-  authDomain: "songlyrically.firebaseapp.com",
-  databaseURL: "https://songlyrically.firebaseio.com",
-  projectId: "songlyrically",
-  storageBucket: "songlyrically.appspot.com",
-  messagingSenderId: "910125473880"
+const config = {
+  apiKey: 'AIzaSyCiLgK-ruR_RYem27X328H3H9bGzujRjn8',
+  authDomain: 'songlyrically.firebaseapp.com',
+  databaseURL: 'https://songlyrically.firebaseio.com',
+  projectId: 'songlyrically',
+  storageBucket: 'songlyrically.appspot.com',
+  messagingSenderId: '910125473880',
 };
 firebase.initializeApp(config);
-
-
