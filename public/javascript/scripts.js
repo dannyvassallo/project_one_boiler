a94a6e1212c32ce1b5453689722e66dc399ae712
JoeySuc89 <jsucameli1@gmail.com>
2019-04-06 10:33:38 -0400

    firebase info on javascript

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,11 @@
-/* eslint-disable */
-console.warn('Project One JS Initialized');
-/* eslint-enable */
+var config = {
+  apiKey: "AIzaSyCiLgK-ruR_RYem27X328H3H9bGzujRjn8",
+  authDomain: "songlyrically.firebaseapp.com",
+  databaseURL: "https://songlyrically.firebaseio.com",
+  projectId: "songlyrically",
+  storageBucket: "songlyrically.appspot.com",
+  messagingSenderId: "910125473880"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
