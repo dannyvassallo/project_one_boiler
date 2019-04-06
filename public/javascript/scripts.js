dc41012c7fd84847c001397bdb64cd68b0b2ed0e
JoeySuc89 <jsucameli1@gmail.com>
2019-04-06 15:38:56 -0400

    start divs

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -46,9 +46,8 @@ $('#submit-button').on ('click', function () {
         method: 'GET',
       }).then((response) => {
         console.log(response);
+        
       });
     }
   getSongs();
 });
-
-// http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=51725924&apikey=09856f0a7bc6623dc9e1a3c333f42318
