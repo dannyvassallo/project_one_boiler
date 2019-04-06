41b81de80a0075582ad3bb85bd9a8ceaa064cb9e
JoeySuc89 <jsucameli1@gmail.com>
2019-04-06 14:05:07 -0400

    added something i believe

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -15,15 +15,9 @@ const yandexBaseUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 const yandexAPIKey =
   'trnsl.1.1.20190406T143223Z.056b981f8fc972d3.8e9576d91e670036aa4c8e89760c7acfa28805f4';
 
-
-
-const translateLang = 'en-ru';
-const rawTranslateText = 'hello this is the text I want to translate';
-const translateText = rawTranslateText.replace (/ /g, '+');
-const queryURL = `${yandexBaseUrl}?key=${yandexAPIKey}&lang=${translateLang}&text=${translateText}&f
 // YANDEX TRANSLATE API
 
-$ ('#submit-button').on ('click', function () {
+$('#submit-button').on ('click', function () {
   event.preventDefault ();
 
   const translateLang = $("#language-input").val();
@@ -44,20 +38,17 @@ $ ('#submit-button').on ('click', function () {
     $ ('#translate-display').append (originalTextDisplay);
     $ ('#translate-display').append (translatedTextDisplay);
   });
+
+    function getSongs(){
+      var youtubeQueryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=artist%7Csongs&safeSearch=moderate&type=video&key=AIzaSyDm3Avv6gF5Xgw2YEm3GB5ILBO5-caJfwU';
+      $.ajax({
+        url:  youtubeQueryUrl,
+        method: 'GET',
+      }).then((response) => {
+        console.log(response);
+      });
+    }
+  getSongs();
 });
 
 // http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=51725924&apikey=09856f0a7bc6623dc9e1a3c333f42318
-
-function getSongs(){
-  var youtubeQueryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + song + '&type=music&key=AIzaSyDm3Avv6gF5Xgw2YEm3GB5ILBO5-caJfwU';
-  var song = 'Ring of Fire'
-  $.ajax({
-    url:  youtubeQueryUrl,
-    method: 'GET',
-  }).then((response) => {
-    console.log(response);
-  });
-
-}
-getSongs();
-
