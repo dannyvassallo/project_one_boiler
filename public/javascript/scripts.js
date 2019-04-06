98b63182ac6b7c0cca7864b6a5434883c190e86f
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-04-06 13:38:34 -0400

    added displays for translator

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -15,12 +15,6 @@ const yandexBaseUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 const yandexAPIKey =
   'trnsl.1.1.20190406T143223Z.056b981f8fc972d3.8e9576d91e670036aa4c8e89760c7acfa28805f4';
 
-
-
-const translateLang = 'en-ru';
-const rawTranslateText = 'hello this is the text I want to translate';
-const translateText = rawTranslateText.replace (/ /g, '+');
-const queryURL = `${yandexBaseUrl}?key=${yandexAPIKey}&lang=${translateLang}&text=${translateText}&f
 // YANDEX TRANSLATE API
 
 $ ('#submit-button').on ('click', function () {
@@ -29,20 +23,22 @@ $ ('#submit-button').on ('click', function () {
   const translateLang = $("#language-input").val();
   const rawTranslateText = $("#translate-text-input").val();
   const translateText = rawTranslateText.replace (/ /g, '+');
-  const queryURL = `${yandexBaseUrl}?key=${yandexAPIKey}&lang=${translateLang}&text=${translateText}&format=plain`;
+  const yandexQueryURL = `${yandexBaseUrl}?key=${yandexAPIKey}&lang=${translateLang}&text=${translateText}&format=plain`;
 
 
   $.ajax ({
-    url: queryURL,
+    url: yandexQueryURL,
     method: 'GET',
   }).then (function (response) {
     const languageDisplay = 'Language: Russian';
-    const originalTextDisplay = 'Original Text: English';
+    const originalTextDisplay = `Original Text:  ${rawTranslateText}`;
     const translatedTextDisplay = `Translated Text: ${response.text}`;
 
-    $ ('#translate-display').append (languageDisplay);
-    $ ('#translate-display').append (originalTextDisplay);
-    $ ('#translate-display').append (translatedTextDisplay);
+    console.log(response.text)
+
+    $ ('#language-display').text (languageDisplay);
+    $ ('#original-text-display').text (originalTextDisplay);
+    $ ('#translated-text-display').text (translatedTextDisplay);
   });
 });
 
@@ -60,4 +56,3 @@ function getSongs(){
 
 }
 getSongs();
-
