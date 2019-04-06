f7409f691c6b15953521e71bae31605f7ce78066
unknown <evan2580@gmail.com>
2019-04-06 13:29:06 -0400

    working api links

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -10,5 +10,36 @@ const config = {
 };
 firebase.initializeApp(config);
 
-// http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=51725924&apikey=09856f0a7bc6623dc9e1a3c333f42318
-=======
+var trackId;
+var trackSearch;
+var artistSearch;
+var lyricsApiKey = "09856f0a7bc6623dc9e1a3c333f42318";
+//var lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackId + "&track.search?q_track=" + trackSearch + "&track.search?q_artist=" + artistSearch + "&apikey=" + lyricsApiKey;
+var lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?q_artist=" + artistSearch + "&apikey=" + lyricsApiKey;
+
+
+
+//THIS IS FOR ARTSIT SEARCH and track
+$.ajax({
+  url: lyricsURL,
+  method: "GET"
+}).then(function(response){
+  console.log(response);
+
+    //THIS IS FOR lyrics
+    $.ajax({
+      url: lyricsURL,
+      method: "GET"
+    }).then(function(response){
+      console.log(response);
+      //THIS IS FOR LYRICS SEARCH
+
+    });
+
+  });
+  
+//http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=123003777&apikey=09856f0a7bc6623dc9e1a3c333f42318
+//gets lyrics with track id
+
+//http://api.musixmatch.com/ws/1.1/track.search?q=justin%20bieber%20Sorry&apikey=09856f0a7bc6623dc9e1a3c333f42318&f_has_lyrics=true
+//pulls artist and track name and if it has lyrics
