4c00e02adcbcfdb21016deefbd8358cb5048c3aa
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-04-06 13:51:45 -0400

    test

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -8,85 +8,80 @@ const config = {
   messagingSenderId: '910125473880',
 };
 
-firebase.initializeApp (config);
+firebase.initializeApp(config);
 
 
 const yandexBaseUrl = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
-const yandexAPIKey =
-  'trnsl.1.1.20190406T143223Z.056b981f8fc972d3.8e9576d91e670036aa4c8e89760c7acfa28805f4';
+const yandexAPIKey = 'trnsl.1.1.20190406T143223Z.056b981f8fc972d3.8e9576d91e670036aa4c8e89760c7acfa28805f4';
 
 // YANDEX TRANSLATE API
 
-$ ('#submit-button').on ('click', function () {
-  event.preventDefault ();
+$('#submit-button').on('click', () => {
+  event.preventDefault();
 
-  const translateLang = $("#language-input").val();
-  const rawTranslateText = $("#translate-text-input").val();
-  const translateText = rawTranslateText.replace (/ /g, '+');
+  const translateLang = $('#language-input').val();
+  const rawTranslateText = $('#translate-text-input').val();
+  const translateText = rawTranslateText.replace(/ /g, '+');
   const yandexQueryURL = `${yandexBaseUrl}?key=${yandexAPIKey}&lang=${translateLang}&text=${translateText}&format=plain`;
 
 
-  $.ajax ({
+  $.ajax({
     url: yandexQueryURL,
     method: 'GET',
-  }).then (function (response) {
+  }).then((response) => {
     const languageDisplay = 'Language: Russian';
     const originalTextDisplay = `Original Text:  ${rawTranslateText}`;
     const translatedTextDisplay = `Translated Text: ${response.text}`;
 
-    console.log(response.text)
+    console.log(response.text);
 
-    $ ('#language-display').text (languageDisplay);
-    $ ('#original-text-display').text (originalTextDisplay);
-    $ ('#translated-text-display').text (translatedTextDisplay);
+    $('#language-display').text(languageDisplay);
+    $('#original-text-display').text(originalTextDisplay);
+    $('#translated-text-display').text(translatedTextDisplay);
   });
 });
 
-var trackId;
-var trackSearch;
-var artistSearch;
-var lyricsApiKey = "09856f0a7bc6623dc9e1a3c333f42318";
-//var lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackId + "&track.search?q_track=" + trackSearch + "&track.search?q_artist=" + artistSearch + "&apikey=" + lyricsApiKey;
-var lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?q_artist=" + artistSearch + "&apikey=" + lyricsApiKey;
+let trackId;
+let trackSearch;
+let artistSearch;
+const lyricsApiKey = '09856f0a7bc6623dc9e1a3c333f42318';
+// var lyricsURL = "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" + trackId + "&track.search?q_track=" + trackSearch + "&track.search?q_artist=" + artistSearch + "&apikey=" + lyricsApiKey;
+const lyricsURL = `http://api.musixmatch.com/ws/1.1/track.lyrics.get?q_artist=${artistSearch}&apikey=${lyricsApiKey}`;
 
 
-
-//THIS IS FOR ARTSIT SEARCH and track
+// THIS IS FOR ARTSIT SEARCH and track
 $.ajax({
   url: lyricsURL,
-  method: "GET"
-}).then(function(response){
+  method: 'GET',
+}).then((response) => {
   console.log(response);
 
-    //THIS IS FOR lyrics
-    $.ajax({
-      url: lyricsURL,
-      method: "GET"
-    }).then(function(response){
-      console.log(response);
-      //THIS IS FOR LYRICS SEARCH
-
-    });
-
+  // THIS IS FOR lyrics
+  $.ajax({
+    url: lyricsURL,
+    method: 'GET',
+  }).then((response) => {
+    console.log(response);
+    // THIS IS FOR LYRICS SEARCH
   });
-  
-//http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=123003777&apikey=09856f0a7bc6623dc9e1a3c333f42318
-//gets lyrics with track id
+});
 
-//http://api.musixmatch.com/ws/1.1/track.search?q=justin%20bieber%20Sorry&apikey=09856f0a7bc6623dc9e1a3c333f42318&f_has_lyrics=true
-//pulls artist and track name and if it has lyrics
+// http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=123003777&apikey=09856f0a7bc6623dc9e1a3c333f42318
+// gets lyrics with track id
+
+// http://api.musixmatch.com/ws/1.1/track.search?q=justin%20bieber%20Sorry&apikey=09856f0a7bc6623dc9e1a3c333f42318&f_has_lyrics=true
+// pulls artist and track name and if it has lyrics
 
 // http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=51725924&apikey=09856f0a7bc6623dc9e1a3c333f42318
 
-function getSongs(){
-  var youtubeQueryUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + song + '&type=music&key=AIzaSyDm3Avv6gF5Xgw2YEm3GB5ILBO5-caJfwU';
-  var song = 'Ring of Fire'
+function getSongs() {
+  const youtubeQueryUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${song}&type=music&key=AIzaSyDm3Avv6gF5Xgw2YEm3GB5ILBO5-caJfwU`;
+  var song = 'Ring of Fire';
   $.ajax({
-    url:  youtubeQueryUrl,
+    url: youtubeQueryUrl,
     method: 'GET',
   }).then((response) => {
     console.log(response);
   });
-
 }
 getSongs();
