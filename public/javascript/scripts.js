30a7711045ce75043d2e6386d6a1175057f3dc0b
unknown <evan2580@gmail.com>
2019-04-06 10:55:36 -0400

    api key

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,3 @@
-/* eslint-disable */
-console.warn('Project One JS Initialized');
-/* eslint-enable */
+
+
+// http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=51725924&apikey=09856f0a7bc6623dc9e1a3c333f42318
