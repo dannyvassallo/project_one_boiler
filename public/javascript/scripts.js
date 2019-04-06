8f4ca79a1300def59bb844d80f9cf4ae92275f74
Austin Smith <austinsmith7531@gmail.com>
2019-04-06 10:10:29 -0400

    skeleton

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,7 @@
 /* eslint-disable */
 console.warn('Project One JS Initialized');
 /* eslint-enable */
+var usernameInput = $("#username-input");
+console.log(usernameInput);
+var passwordInput = $("#password-input");
+console.log(passwordInput);
