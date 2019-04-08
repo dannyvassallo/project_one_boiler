313e6a72b749ebced5daac0ee6c77ce62f0bf33f
Markus Maasikas <markus@hotmail.com>
2019-04-08 19:28:21 -0400

    ajax

--- public/javascript/ajax.js
+++ public/javascript/ajax.js
@@ -1,32 +1,32 @@
 
-// var queryURL = "https://api.eventful.com/rest/events/get?" + "&limit=10&lang=en";
-// queryURL += "&api_key=sVJJH5rghHKW4MpB";
-// //   // Performing an AJAX request with the queryURL
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// })
-// //After data comes back from the request
-// .then(function (response) {
-//   // console.log(queryURL);
-//   var results = response.data;
-//   console.log(response);
-//   console.log(event.url);
-// }
-// clear eveyrthing before it adds additonal images
-// $("#eventName").empty();
-// //Looping through each result item
-// for (var i = 0; i < results.length; i++) {
-//   // Creating and storing a div tag
-//   var eventName = $("<div class='col-sm-12 col-md-4'>");
-//   //   // Creating a paragraph tag with the result item's rating
-//   var p = $("<p>").text("artistName: " + results[i].artistName);
-//   //  Creating and storing an image tag
-//   var results = $("<img>");
+var queryURL = "https://api.eventful.com/rest/events/get?" + "&limit=10&lang=en";
+queryURL += "&api_key=sVJJH5rghHKW4MpB";
+//   // Performing an AJAX request with the queryURL
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+//After data comes back from the request
+.then(function (response) {
+  // console.log(queryURL);
+  var results = response.data;
+  console.log(response);
+  console.log(event.url);
+}
+clear eveyrthing before it adds additonal images
+$("#eventName").empty();
+//Looping through each result item
+for (var i = 0; i < results.length; i++) {
+  // Creating and storing a div tag
+  var eventName = $("<div class='col-sm-12 col-md-4'>");
+  //   // Creating a paragraph tag with the result item's rating
+  var p = $("<p>").text("artistName: " + results[i].artistName);
+  //  Creating and storing an image tag
+  var results = $("<img>");
 
-// });
+});
 
-//   if
+  if
 
 
-//   else
+  else
