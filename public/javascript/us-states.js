44b8643dc5cddf299ce0ccf77c2048554c315dd1
ko513 <46111791+ko513@users.noreply.github.com>
2019-04-09 19:46:03 -0400

    Latest map updates

---
+++ public/javascript/us-states.js
@@ -0,0 +1,253 @@
+
+  var states = [
+  { "name": "Alabama",
+    "abbreviation": "AL",
+    "capital": "Montgomery",
+    "answered": false },
+
+  { "name": "Alaska",
+    "abbreviation": "AK",
+    "capital": "Juneau",
+    "answered": false },
+
+  { "name": "Arizona",
+    "abbreviation": "AZ",
+    "capital": "Phoenix",
+    "answered": false },
+
+  { "name": "Arkansas",
+    "abbreviation": "AR",
+    "capital": "Little Rock",
+    "answered": false },
+
+  { "name": "California",
+    "abbreviation": "CA",
+    "capital": "Sacramento",
+    "answered": false },
+
+  { "name": "Colorado",
+    "abbreviation": "CO",
+    "capital": "Denver",
+    "answered": false },
+
+  { "name": "Connecticut",
+    "abbreviation": "CT",
+    "capital": "Hartford",
+    "answered": false },
+
+  { "name": "Delaware",
+    "abbreviation": "DE",
+    "capital": "Dover",
+    "answered": false },
+
+  { "name": "Florida",
+    "abbreviation": "FL",
+    "capital": "Tallahassee",
+    "answered": false },
+
+  { "name": "Georgia",
+    "abbreviation": "GA",
+    "capital": "Atlanta",
+    "answered": false },
+
+  { "name": "Hawaii",
+    "abbreviation": "HI",
+    "capital": "Honolulu",
+    "answered": false },
+
+  { "name": "Idaho",
+    "abbreviation": "ID",
+    "capital": "Boise",
+    "answered": false },
+
+  { "name": "Illinois",
+    "abbreviation": "IL",
+    "capital": "Springfield",
+    "answered": false },
+
+  { "name": "Indiana",
+    "abbreviation": "IN",
+    "capital": "Indianapolis",
+    "answered": false },
+
+  { "name": "Iowa",
+    "abbreviation": "IA",
+    "capital": "Des Moines",
+    "answered": false },
+
+  { "name": "Kansas",
+    "abbreviation": "KS",
+    "capital": "Topeka",
+    "answered": false },
+
+  { "name": "Kentucky",
+    "abbreviation": "KY",
+    "capital": "Frankfort",
+    "answered": false },
+
+  { "name": "Louisiana",
+    "abbreviation": "LA",
+    "capital": "Baton Rouge",
+    "answered": false },
+
+  { "name": "Maine",
+    "abbreviation": "ME",
+    "capital": "Augusta",
+    "answered": false },
+
+  { "name": "Maryland",
+    "abbreviation": "MD",
+    "capital": "Annapolis",
+    "answered": false },
+
+  { "name": "Massachusetts",
+    "abbreviation": "MA",
+    "capital": "Boston",
+    "answered": false },
+
+  { "name": "Michigan",
+    "abbreviation": "MI",
+    "capital": "Lansing",
+    "answered": false },
+
+  { "name": "Minnesota",
+    "abbreviation": "MN",
+    "capital": "Saint Paul",
+    "answered": false },
+
+  { "name": "Mississippi",
+    "abbreviation": "MS",
+    "capital": "Jackson",
+    "answered": false },
+
+  { "name": "Missouri",
+    "abbreviation": "MO",
+    "capital": "Jefferson City",
+    "answered": false },
+
+  { "name": "Montana",
+    "abbreviation": "MT",
+    "capital": "Helena",
+    "answered": false },
+
+  { "name": "Nebraska",
+    "abbreviation": "NE",
+    "capital": "Lincoln",
+    "answered": false },
+
+  { "name": "Nevada",
+    "abbreviation": "NV",
+    "capital": "Carson City",
+    "answered": false },
+
+  { "name": "New Hampshire",
+    "abbreviation": "NH",
+    "capital": "Concord",
+    "answered": false },
+
+  { "name": "New Jersey",
+    "abbreviation": "NJ",
+    "capital": "Trenton",
+    "answered": false },
+
+  { "name": "New Mexico",
+    "abbreviation": "NM",
+    "capital": "Santa Fe",
+    "answered": false },
+
+  { "name": "New York",
+    "abbreviation": "NY",
+    "capital": "Albany",
+    "answered": false },
+
+  { "name": "North Carolina",
+    "abbreviation": "NC",
+    "capital": "Raleigh",
+    "answered": false },
+
+  { "name": "North Dakota",
+    "abbreviation": "ND",
+    "capital": "Bismarck",
+    "answered": false },
+
+  { "name": "Ohio",
+    "abbreviation": "OH",
+    "capital": "Columbus",
+    "answered": false },
+
+  { "name": "Oklahoma",
+    "abbreviation": "OK",
+    "capital": "Oklahoma City",
+    "answered": false },
+
+  { "name": "Oregon",
+    "abbreviation": "OR",
+    "capital": "Salem",
+    "answered": false },
+
+  { "name": "Pennsylvania",
+    "abbreviation": "PA",
+    "capital": "Harrisburg",
+    "answered": false },
+
+  { "name": "Rhode Island",
+    "abbreviation": "RI",
+    "capital": "Providence",
+    "answered": false },
+
+  { "name": "South Carolina",
+    "abbreviation": "SC",
+    "capital": "Charleston",
+    "answered": false },
+
+  { "name": "South Dakota",
+    "abbreviation": "SD",
+    "capital": "Pierre",
+    "answered": false },
+
+  { "name": "Tennessee",
+    "abbreviation": "TN",
+    "capital": "Nashville",
+    "answered": false },
+
+  { "name": "Texas",
+    "abbreviation": "TX",
+    "capital": "Austin",
+    "answered": false },
+
+  { "name": "Utah",
+    "abbreviation": "UT",
+    "capital": "Salt Lake City",
+    "answered": false },
+
+  { "name": "Vermont",
+    "abbreviation": "VT",
+    "capital": "Montpelier",
+    "answered": false },
+
+  { "name": "Virginia",
+    "abbreviation": "VA",
+    "capital": "Richmond",
+    "answered": false },
+
+  { "name": "Washington",
+    "abbreviation": "WA",
+    "capital": "Olympia",
+    "answered": false },
+
+  { "name": "West Virginia",
+    "abbreviation": "WV",
+    "capital": "Charleston",
+    "answered": false },
+
+  { "name": "Wisconsin",
+    "abbreviation": "WI",
+    "capital": "Madison",
+    "answered": false },
+
+  { "name": "Wyoming",
+    "abbreviation": "WY",
+    "capital": "Cheyenne",
+    "answered": false
+  }
+]
