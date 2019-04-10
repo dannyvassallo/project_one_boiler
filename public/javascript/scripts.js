24f3ae03cf5deed42c4d1a76a052b8482d72f285
Mhrpatel85 <29385964+Mhr85@users.noreply.github.com>
2019-04-09 20:50:40 -0400

    fixed conflicts, added login, signup & forgot password html, created firebase auth & database

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -17,60 +17,60 @@ $(document).ready(() => {
     const name = $('#name').val().trim();
     const number = $('#ph-number').val().trim();
     const zip = $('#zip-code').val().trim();
-    var newUser = {
+    const newUser = {
       userName: name,
       phNumber: number,
-      zipCode: zip
+      zipCode: zip,
     };
-    const user = firebase.auth().currentUser
-    //console.log(newUser);
-    //const myPassword = 'Password';
-    //var password = CryptoJS.AES.encrypt($('#exampleInputPassword1').val().trim(), myPassword);
-    //const res = String(password);
+    const user = firebase.auth().currentUser;
+    // console.log(newUser);
+    // const myPassword = 'Password';
+    // var password = CryptoJS.AES.encrypt($('#exampleInputPassword1').val().trim(), myPassword);
+    // const res = String(password);
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        if (user) {
-          firebase.auth().signOut()
-          window.location = 'http://localhost:3000/login';
-        }
-      })
+      // .then((userCredential) => {
+      //   if (user) {
+      //     firebase.auth().signOut();
+      //     window.location = 'http://localhost:3000/login';
+      //   }
+      //})
       .catch((error) => {
-        //const errorToast = error;
+        // const errorToast = error;
         $('#signup-error').text(error.message);
-      })
+      });
 
-    firebase.database().ref("/users/" + user.uid).child("/profile").push(newUser);
+    firebase.database().ref(`/users/${user.uid}`).child('/profile').push(newUser);
 
     $('#exampleInputEmail1').val('');
     $('#exampleInputPassword1').val('');
     $('#first-name').val('');
     $('#last-name').val('');
-    $('#name').val("");
-    $('#ph-number').val("");
-    $('#zip-code').val("");
+    $('#name').val('');
+    $('#ph-number').val('');
+    $('#zip-code').val('');
   });
 
   $('#login').on('click', (event) => {
     event.preventDefault();
     const email = $('#exampleInputEmail1').val().trim();
     const password = $('#exampleInputPassword1').val().trim();
-    //const myPassword = 'Password';
-    //var password = CryptoJS.AES.encrypt($('#exampleInputPassword1').val().trim(), myPassword);
-    //const resLogin = String(password);
-    //const user = firebase.auth().currentUser;
+    // const myPassword = 'Password';
+    // var password = CryptoJS.AES.encrypt($('#exampleInputPassword1').val().trim(), myPassword);
+    // const resLogin = String(password);
+    // const user = firebase.auth().currentUser;
 
     firebase.auth().signInWithEmailAndPassword(email, password)
-      // .then(function(user) {
-      //   if(user){
-      //     window.location = "http://localhost:3000"
-      //   }
-      // })
+    // .then(function(user) {
+    //   if(user){
+    //     window.location = "http://localhost:3000"
+    //   }
+    // })
 
       .catch((error) => {
         const errorToast = error;
         $('#signin-error').text(errorToast.message);
-        //location.reload();
+        // location.reload();
       });
 
     $('#exampleInputEmail1').val('');
@@ -82,11 +82,11 @@ $(document).ready(() => {
     const auth = firebase.auth();
     const emailAddress = $('#exampleInputEmail1').val().trim();
 
-    //auth.sendPasswordResetEmail(emailAddress).then(() => {
-      // Email sent.
-    //}).catch((error) => {
-      // An error happened.
-    //});
+    auth.sendPasswordResetEmail(emailAddress).then(() => {
+    // Email sent.
+    // }).catch((error) => {
+    // An error happened.
+    });
     $('#exampleInputEmail1').val('');
   });
 
@@ -94,31 +94,31 @@ $(document).ready(() => {
     event.preventDefault();
     firebase.auth().signOut().then(() => {
       // Sign-out successful.
-    //}).catch((error) => {
+    // }).catch((error) => {
       // An error happened.
-    //});
+    // });
 
       const user = firebase.auth().currentUser;
       if (user) {
-        //console.log(user);
+        // console.log(user);
         window.location = 'http://localhost:3000/login';
       }
     });
   });
 
   firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      //User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      console.log(user);
-      console.log(email);
-    } else {
-    }
+    // if (user) {
+    //   // User is signed in.
+    //   const { displayName } = user;
+    //   const { email } = user;
+    //   const { emailVerified } = user;
+    //   const { photoURL } = user;
+    //   const { isAnonymous } = user;
+    //   const { uid } = user;
+    //   const { providerData } = user;
+    //   console.log(user);
+    //   console.log(email);
+    // } else {
+    // }
   });
 });
