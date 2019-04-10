8cc96f20a05c5781d1a0cddbc0d1da064627d8b3
DavidmB <david.m.bedford1@gmail.com>
2019-04-09 20:44:30 -0400

    cards one hundred percent

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -14,7 +14,6 @@ function loadVideoBackground() {
   const bv = new Bideo(); // eslint-disable-line no-undef
   bv.init({
     // Video element
-
     videoEl: document.querySelector('#background_video'),
 
     // Container element
@@ -47,144 +46,6 @@ function loadVideoBackground() {
 
 loadVideoBackground();
 
-/* eslint-disable */
-// function createBlocks (event) {
-//  //tier 4
-//   var newCardBody = $("<div class='card-body'>");
-//   $("#tableZone > .card").append(newCardBody);
-//  //tier 5
-//   var table = $("<table class='table'>");
-//   $(".card-body").append(newCardHeader);
-//  //tier 6
-//   var tableHead = $("<thead>");
-//   $(".table").append(tableHead);
-//  //tier 7
-//   var topRow = $("<tr>").append(
-//    //tier 8
-//     $("<td>").text("Date"),
-//     $("<td>").text("Event"),
-//     $("<td>").text("Venue Name"),
-//     $("<td>").text("City/Town"),
-//     $("<td>").text("Buttons")
-//       );
-//    //tier 6
-//     $("<thead>").append(topRow);
-//    //tier 5
-//     table.append(tableHead);
-//    //tier 6
-//     var tableBody = $("<tbody>");
-//    //tier 7
-//     var resultRow = $("<tr>").append(
-//      //tier 8
-//         $("<td>").text(this.eventDate),
-//         $("<td>").text(this.eventName),
-//         $("<td>").text(this.eventVenueName),
-//         $("<td>").text("cityState"),
-//         $("<td>").text("Add-to-Profile Button"),
-//       );
-//     //  $("#tableZone > .card").append(newCardHeader);
-//  //tier 6
-//   tableBody.append(resultRow);
-//  //tier 5
-//   table.append(tableBody);
-//  //tier 4
-//   newCardBody.append(table);
-
-function createBlocks () {
-  //
-  let newCard = $("<div class='card'>");
-  $("#tableZone").append(newCard);
-  //
-  let cardHeader = $("<div class='card-header'>");
-  $(".card").append(cardHeader);
-  $(".card").text(eventTitle);
-  //
-  let cardBody = $("<div class='card-body'>");
-  $(".card-header").append(cardBody);
-  //
-  let button = $("<button>");
-  button.attr("type", "button");
-  button.attr("class", "btn btn-primary");
-  button.attr("data-toggle", "modal");
-  button.attr("data-target", "#exampleModal");
-  button.text("Modal");
-  //tier 7
-  let info = $("<div>").append(
-  //tier 8
-  $("<p>").text(this.eventDate),
-  $("<p>").text(this.eventTime),
-  $("<p>").text(this.eventVenueName),
-  $("<div>").append(button)
-    );
-  //
-  $(".card-body").append(info);
-   };
-
-/* eslint-disable */
-
-/* eslint-disable */
-function requestTicketmaster() {
-  let myUrl = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=IOLEuwOBaextS3XP3HR0L3NUcF3eaFqf&keyword=${eventName}&postalCode=${userLocation}&radius=${userRange}&unit=miles`;
-  console.log("API URL, Notice its broken if inout is spaced: ", myUrl);
-
-  $.ajax({
-    url: myUrl,
-    method: 'GET',
-  }).then(function(response) {
-    let responseX = response['_embedded'];
-    console.log("responseX: ", responseX);
-  for (let i = 0; i < responseX.events.length; i += 1) {
-    let eventTitle = responseX.events[i].name;
-    console.log("eventTitle: ", eventTitle);
-    let eventImg = responseX.events[i].images;
-    console.log("eventImg: ", eventImg);
-    let eventDate = responseX.events[i].dates.start.localDate;
-    console.log("eventDate: ", eventDate);
-    let eventTime = responseX.events[i].dates.start.localTime;
-    console.log("eventTime: ", eventTime);
-    let eventUrl = responseX.events[i].url;
-    console.log("eventUrl: ", eventUrl);
-    let eventVenueName = responseX.events[i]['_embedded'].venues[0].name;
-    console.log("eventVenueName: ", eventVenueName);
-    let eventVenueLong = responseX.events[i]['_embedded'].venues[0].location.longitude;
-    console.log("eventVenueLong: ", eventVenueLong);
-    let eventVenueLati = responseX.events[i]['_embedded'].venues[0].location.latitude;
-    console.log("eventVenueLati", eventVenueLati);
-
-    //
-
-    let newCard = $("<div class='card'>");
-    $("#cardZone").append(newCard);
-    //
-    let cardHeader = $("<div class='card-header'>");
-    $(".card").append(cardHeader);
-    $(".card-header").text(eventTitle);
-    //
-    let cardBody = $("<div class='card-body'>");
-    $(".card-header").append(cardBody);
-    //
-    let button = $("<button>");
-    button.attr("type", "button");
-    button.attr("class", "btn btn-primary");
-    button.attr("data-toggle", "modal");
-    button.attr("data-target", "#exampleModal");
-    button.text("Modal");
-    //tier 7
-    let info = $("<div>").append(
-    //tier 8
-    $("<p>").text(eventDate),
-    $("<p>").text(eventTime),
-    $("<p>").text(eventVenueName),
-    $("<div>").append(button)
-      );
-    //
-    $(".card-body").append(info);
-     };
-    }
-  )
-};
-
-
 function retrieveForm(event) {
   event.preventDefault();
 
@@ -199,8 +60,69 @@ function retrieveForm(event) {
   formBlock = [eventName, userLocation, userRange];
   console.log(formBlock);
 
-
   requestTicketmaster(); /* eslint-enable */
 }
+/* eslint-disable */
+function requestTicketmaster() {
+  $("#cardZone").empty();
+  let myUrl = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=IOLEuwOBaextS3XP3HR0L3NUcF3eaFqf&keyword=${eventName}&postalCode=${userLocation}&radius=${userRange}&unit=miles`;
+  console.log("API URL, Notice its broken if inout is spaced: ", myUrl);
+
+  $.ajax({
+    url: myUrl,
+    method: 'GET',
+  }).then(function(response) {
+    let responseX = response['_embedded'];
+    console.log("responseX: ", responseX);
+    const events = responseX.events
+
+    $.each(events, function(index, event){
+      let eventTitle = event.name;
+      console.log("eventTitle: ", eventTitle);
+      let eventImg = event.images;
+      console.log("eventImg: ", eventImg);
+      let eventDate = event.dates.start.localDate;
+      console.log("eventDate: ", eventDate);
+      let eventTime = event.dates.start.localTime;
+      console.log("eventTime: ", eventTime);
+      let eventUrl = event.url;
+      console.log("eventUrl: ", eventUrl);
+      let eventVenueName = event['_embedded'].venues[0].name;
+      console.log("eventVenueName: ", eventVenueName);
+      let eventVenueLong = event['_embedded'].venues[0].location.longitude;
+      console.log("eventVenueLong: ", eventVenueLong);
+      let eventVenueLati = event['_embedded'].venues[0].location.latitude;
+      console.log("eventVenueLati", eventVenueLati);
+
+
+      let newCard = $("<div class='card'>");
+      $("#cardZone").append(newCard);
+      //
+      let cardHeader = $("<div class='card-header'>");
+      newCard.append(cardHeader);
+      cardHeader.text(eventTitle);
+      //
+      let cardBody = $("<div class='card-body'>");
+      cardHeader.append(cardBody);
+      //
+      let button = $("<button>");
+      button.attr("type", "button");
+      button.attr("class", "btn btn-primary");
+      button.attr("data-toggle", "modal");
+      button.attr("data-target", "#exampleModal");
+      button.text("More Info");
+      //tier 7
+      let info = $("<div>").append(
+      //tier 8
+      $("<p>").text(eventDate),
+      $("<p>").text(eventTime),
+      $("<p>").text(eventVenueName),
+      $("<div>").append(button)
+        );
+      //
+      cardBody.append(info);
+    })
+  })
+};
 
 $('#submit-btn').on('click', retrieveForm);
